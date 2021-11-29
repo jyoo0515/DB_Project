c0040db79b23330749d50a332cb9c76d1c4ef544
C-limlim <cofla2020@yonsei.ac.kr>
2021-11-30 02:22:45 +0900

    211130 FriendSearch

--- client/src/components/views/App.js
+++ client/src/components/views/App.js
@@ -4,12 +4,14 @@ import { LandingPage } from "./LandingPage/LandingPage";
 import { LoginPage } from "./LoginPage/LoginPage";
 import { RegisterPage } from "./RegisterPage/RegisterPage";
 import { NavBar } from "./NavBar/NavBar";
+import { FriendSearchPage } from "./FriendSearch/FriendSearch";
 
 export const App = () => {
   return (
     <div>
       <NavBar />
       <Switch>
+        <Route exact path="/friendsearch" component={FriendSearchPage} />
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/register" component={RegisterPage} />
         <Route exact path="/login" component={LoginPage} />
