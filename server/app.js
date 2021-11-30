e842551f146247a32768c9b4bd20091fde24ebcf
jyoo0515 <jasonyoo0515@gmail.com>
2021-12-01 01:33:41 +0900

    refactor: app.js

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
