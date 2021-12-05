c76c6fdd351311b29cb7697a4c320655969fba17
jyoo0515 <jasonyoo0515@gmail.com>
2021-12-06 03:29:19 +0900

    feature: hoc for path control

---
+++ client/src/hoc/auth.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import apiClient from "../components/utils/axios";
+
+export default function (SpecificComponent, loginRequired) {
+  function AuthenticationCheck(props) {
+    let user;
+    let isAuth = false;
+
+    useEffect(() => {
+      apiClient
+        .get("/users/me")
+        .then((response) => {
+          user = response.data;
+          isAuth = true;
+        })
+        .catch((err) => {
+          user = undefined;
+          isAuth = false;
+        });
+
+      if (!isAuth) {
+        if (loginRequired) {
+          props.history.push("/login");
+        }
+      } else {
+        if (loginRequired == null) {
+          props.history.push("/");
+        }
+      }
+    }, []);
+
+    return (
+      // <SpecificComponent {...props} user={user} />
+      <SpecificComponent {...props} />
+    );
+  }
+  return AuthenticationCheck;
+}
