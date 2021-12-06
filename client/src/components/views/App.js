52b20275edd613f554613e793a0e18212e01fdad
KWONDU <go203302@gmail.com>
2021-12-06 16:15:20 +0900

    211206 16:15

--- client/src/components/views/App.js
+++ client/src/components/views/App.js
@@ -5,26 +5,28 @@ import { LandingPage } from "./LandingPage/LandingPage";
 import { LoginPage } from "./LoginPage/LoginPage";
 import { RegisterPage } from "./RegisterPage/RegisterPage";
 import { ChatList } from "./ChatList/ChatList";
-import { NearbyPeople } from "./NearbyPeople/NearbyPeople";
+import { Nearby } from "./Nearby/Nearby";
 import { FriendSearchPage } from "./FriendSearchPage/FriendSearch";
 import { FriendListPage } from "./FriendListPage/FriendList";
 import { EditPage } from "./EditPage/EditPage";
 import { Socket } from "./Socket/Socket";
+import { NearbyPeople } from "./Nearby/NearbyPeople";
 
 export const App = () => {
   return (
     <div>
       <Switch>
-        <Route exact path="/chats/:roomId" component={ChatRoomPage} />
-        <Route exact path="/search" component={FriendSearchPage} />
-        <Route exact path="/friends" component={FriendListPage} />
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/register" component={RegisterPage} />
         <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/chats" component={ChatList} />
-        <Route exact path="/nearby/people" component={NearbyPeople} />
         <Route exact path="/edit" component={EditPage} />
-        <Route exct path="/socket/:roomId" component={Socket} />
+        <Route exact path="/friends" component={FriendListPage} />
+        <Route exact path="/search" component={FriendSearchPage} />
+        <Route exact path="/nearby" component={Nearby} />
+        <Route exact path="/nearby/people/:locId" component={NearbyPeople} />
+        <Route exact path="/chats" component={ChatList} />
+        <Route exact path="/chats/:roomId" component={ChatRoomPage} />
+        <Route exact path="/socket/:roomId" component={Socket} />
       </Switch>
     </div>
   );
