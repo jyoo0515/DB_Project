e6ed0112445ccf998317df7b2bebea8b162e605c
KWONDU <go203302@gmail.com>
2021-12-07 03:31:15 +0900

    211207 03:31

--- client/src/components/views/EditPage/EditPage.jsx
+++ client/src/components/views/EditPage/EditPage.jsx
@@ -4,8 +4,15 @@ import "./editStyle.css";
 
 export const EditPage = () => {
   const [myData, setMyData] = useState({});
+  const [statusMessage, setStatusMessage] = useState("");
+  const [location, setLocation] = useState("");
+
   useEffect(() => {
-    apiClient.get("/users/me").then((res) => setMyData(res.data));
+    apiClient.get("/users/me").then((res) => {
+      setMyData(res.data);
+      setStatusMessage(res.data.statusMessage);
+      setLocation(res.data.location);
+    });
   }, []);
 
   const logOut = () => {
@@ -14,26 +21,61 @@ export const EditPage = () => {
   };
 
   const deleteAccount = () => {
-    apiClient
-      .delete("/users/me")
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+    apiClient.delete("/users/me").catch((err) => console.log(err));
     document.location.href = "/";
+  };
+
+  const onChangeInput = (e) => {
+    setStatusMessage(e.target.value);
+  };
+
+  const onChangeSelect = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (myData.statusMessage === statusMessage && myData.location === location) return;
+    if (myData.statusMessage !== statusMessage)
+      apiClient
+        .patch("/users/me", {
+          statusMessage: statusMessage,
+          location: null,
+        })
+        .catch((err) => console.log(err));
+    if (myData.location !== location)
+      apiClient
+        .patch("/users/me", {
+          statusMessage: null,
+          location: location,
+        })
+        .catch((err) => console.log(err));
+    document.location.href = "/edit";
+  };
+
+  const returnToFriends = (e) => {
+    e.preventDefault();
+    document.location.href = "/friends";
   };
 
   return (
     <div>
       <div className="content">
+        <div className="returnToFriends">
+          <img src="../../../../leftArrow.png" className="returnImg" onClick={returnToFriends} />
+        </div>
         <div className="edit"> EDIT </div>
         <div className="blackLine1"></div>
-        <form className="formstyle">
-          a<div className="word">STATUS MESSAGE</div>
+        <form className="formstyle" onSubmit={onSubmit}>
+          <div className="word">STATUS MESSAGE</div>
           <input
             className="enterBox"
-            placeholder={myData.statusMessage === "null" ? "상태 메세지를 입력하세요" : myData.statusMessage}
+            placeholder={statusMessage === "null" || statusMessage === "" ? "상태 메세지를 입력하세요" : ""}
+            onChange={onChangeInput}
+            value={statusMessage}
           ></input>
           <div className="word">LOCATION</div>
-          <select className="enterBox" defaultValue="공학관">
+          <select className="enterBox" defaultValue={myData.location} onChange={onChangeSelect} value={location}>
             <option className="location" value="공학관">
               공학관
             </option>
