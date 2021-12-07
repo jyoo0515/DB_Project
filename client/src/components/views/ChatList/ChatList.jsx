90efbc7594db8f0c8abc31db4796c0f7e6feed17
2020147569 <pom0319@gmail.com>
2021-12-07 22:41:49 +0900

    ChatList visualization

--- client/src/components/views/ChatList/ChatList.jsx
+++ client/src/components/views/ChatList/ChatList.jsx
@@ -3,10 +3,6 @@ import "./ChatList.css";
 import apiClient from "../../utils/axios";
 
 export const ChatList = () => {
-  const [myData, setMyData] = useState({});
-  useEffect(() => {
-    apiClient.get("/chats").then((res) => setMyData(res.data));
-  }, []);
   return (
     <div>
       <div className="container">
@@ -22,31 +18,32 @@ export const ChatList = () => {
 };
 
 function Friend() {
+  const [myData, setMyData] = useState({});
+  useEffect(() => {
+    apiClient.get("/chats").then((res) => setMyData(res));
+  }, []);
   const result = [];
-  apiClient.get("/chats").then(function (res) {
-    console.log(res.status);
-    console.log(res.data);
-    if (res.status != 200) {
-      return <div>잘못된 요청입니다</div>;
-    }
-    console.log(Object.keys(res.data).length);
-    if (!Object.keys(res.data).length) {
-      console.log("no data");
-      return <div>생성된 채팅방이 없습니다.</div>;
-    }
-    for (const chats in res.data) {
-      result.push(
-        <div className="friend">
-          <div>{chats.friendId}</div>
-          <div style={{ display: "flex" }}>
-            <div>채팅 메시지</div>
-            <div>{chats.lastOnline}</div>
-          </div>
-          <button className="ButtonStyle">채팅</button>
+  console.log(myData.status);
+  console.log(myData.data);
+  if (myData.status != 200) {
+    return <div>잘못된 요청입니다</div>;
+  }
+  console.log(Object.keys(myData.data).length);
+  if (!Object.keys(myData.data).length) {
+    console.log("no data");
+    return <div>생성된 채팅방이 없습니다.</div>;
+  }
+  for (const chats in myData.data) {
+    result.push(
+      <div className="friend">
+        <div>{chats.friendId}</div>
+        <div style={{ display: "flex" }}>
+          <div>채팅 메시지</div>
+          <div>{chats.lastOnline}</div>
         </div>
-      );
-    }
-    return result;
-  });
+        <button className="ButtonStyle">채팅</button>
+      </div>
+    );
+  }
   return result;
 }
