f223d4bf19234e9a81f08593afb6a136c1fbda15
jyoo0515 <jasonyoo0515@gmail.com>
2021-12-08 19:59:03 +0900

    update: apiClient base url

--- client/src/components/utils/axios.js
+++ client/src/components/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   // baseURL: process.env.REACT_APP_API_BASEURL,
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.API_URL || "http://localhost:5000/api",
   timeout: 5000,
   withCredentials: true,
   headers: {
