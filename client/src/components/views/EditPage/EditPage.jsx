00132caba3d39d3ffd6c6739aff73f4ed15a1a65
minguni <mingguniii@yonsei.ac.kr>
2021-12-09 02:31:15 +0900

    나 완전 다 끝난둣?

--- client/src/components/views/EditPage/EditPage.jsx
+++ client/src/components/views/EditPage/EditPage.jsx
@@ -5,13 +5,20 @@ import "./editStyle.css";
 
 export const EditPage = () => {
   const [myData, setMyData] = useState({});
+  const [statusMessage, setStatus] = useState("");
+  const [location, setLocation] = useState("");
+
   useEffect(() => {
-    apiClient.get("/users/me").then((res) => setMyData(res.data));
+    apiClient.get("/users/me").then((res) => {
+      setMyData(res.data);
+      setStatus(res.data.statusMessage);
+      setLocation(res.data.location);
+    });
   }, []);
 
   const logOut = () => {
     apiClient.get("/users/logout").catch((err) => console.log(err));
-    document.location.href = "/";
+    document.location.href = "/login";
   };
 
   const deleteAccount = () => {
@@ -19,40 +26,62 @@ export const EditPage = () => {
       .delete("/users/me")
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
-    document.location.href = "/";
+    document.location.href = "/login";
+  };
+
+  const onStatusChange = (event) => {
+    setStatus(event.target.value);
+  };
+
+  const onLocationChange = (event) => {
+    setLocation(event.target.value);
   };
 
   const editAccount = (event) => {
     event.preventDefault();
-    const editData = new FormData(event.currentTarget);
-    const editStatus = {
-      statusMessage: editData.get("editStatus"),
+
+    const statusMessageGroup = {
+      statusMessage,
       location: null,
     };
 
-    const editLocation = {
+    const locationGroup = {
       statusMessage: null,
-      location: editData.get("editLocation"),
+      location,
     };
 
-    apiClient.patch("/users/me", editStatus).then((res) => console.log(res.data));
-    apiClient.patch("/users/me", editLocation).then((res) => console.log(res.data));
+    if (statusMessage.length > 20) {
+      alert("상태 메세지는 최대 20자로 변경 가능합니다.");
+    } else {
+      if (statusMessage !== "") {
+        apiClient.patch("/users/me", statusMessageGroup).then((res) => console.log(res.data));
+      }
+      if (location !== "") {
+        apiClient.patch("/users/me", locationGroup).then((res) => console.log(res.data));
+      }
+      document.location.href = "/edit";
+    }
   };
 
   return (
     <div>
       <div className="content">
+        <a href="/friends">
+          <img className="leftArr" src="leftArrow.png" />
+        </a>
         <div className="edit"> EDIT </div>
         <div className="blackLine1"></div>
         <form className="formstyle" onSubmit={editAccount}>
           <div className="word">STATUS MESSAGE</div>
           <input
+            onChange={onStatusChange}
             className="enterBox"
             placeholder={myData.statusMessage === "null" ? "상태 메세지를 입력하세요" : myData.statusMessage}
             id="editStatus"
           ></input>
+          <div className="checkText">{statusMessage.length}/20</div>
           <div className="word">LOCATION</div>
-          <select className="enterBox" defaultValue="공학관" id="editLocation">
+          <select className="enterBox" onChange={onLocationChange} value={location} defaultValue={myData.location}>
             <option className="location" value="공학관">
               공학관
             </option>
