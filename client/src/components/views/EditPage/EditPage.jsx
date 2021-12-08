0424ba8ece8ee4eca04333125d43218ea106cc89
minguni <mingguniii@yonsei.ac.kr>
2021-12-08 22:46:08 +0900

    귀요미 동현이

--- client/src/components/views/EditPage/EditPage.jsx
+++ client/src/components/views/EditPage/EditPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { StrictEventEmitter } from "socket.io/dist/typed-events";
 import apiClient from "../../utils/axios";
 import "./editStyle.css";
 
@@ -21,19 +22,37 @@ export const EditPage = () => {
     document.location.href = "/";
   };
 
+  const editAccount = (event) => {
+    event.preventDefault();
+    const editData = new FormData(event.currentTarget);
+    const editStatus = {
+      statusMessage: editData.get("editStatus"),
+      location: null,
+    };
+
+    const editLocation = {
+      statusMessage: null,
+      location: editData.get("editLocation"),
+    };
+
+    apiClient.patch("/users/me", editStatus).then((res) => console.log(res.data));
+    apiClient.patch("/users/me", editLocation).then((res) => console.log(res.data));
+  };
+
   return (
     <div>
       <div className="content">
         <div className="edit"> EDIT </div>
         <div className="blackLine1"></div>
-        <form className="formstyle">
-          a<div className="word">STATUS MESSAGE</div>
+        <form className="formstyle" onSubmit={editAccount}>
+          <div className="word">STATUS MESSAGE</div>
           <input
             className="enterBox"
             placeholder={myData.statusMessage === "null" ? "상태 메세지를 입력하세요" : myData.statusMessage}
+            id="editStatus"
           ></input>
           <div className="word">LOCATION</div>
-          <select className="enterBox" defaultValue="공학관">
+          <select className="enterBox" defaultValue="공학관" id="editLocation">
             <option className="location" value="공학관">
               공학관
             </option>
